16cbee84581425435501aa6aa667ce41cc28eb72
angvishvish <angvish@promobitech.com>
2015-06-20 00:44:30 +0530

    signup and login working

--- passport-init.js
+++ passport-init.js
@@ -1,89 +1,48 @@
+var mongoose = require('mongoose');   
+var User = mongoose.model('User');
 var LocalStrategy   = require('passport-local').Strategy;
-var bCrypt          = require('bcrypt-nodejs');
+var bCrypt = require('bcrypt-nodejs');
 
-//temporary data store
-// var user = {};
-
-var mongoose    =  require('mongoose');
-var User        = mongoose.model('User');
-var Post        = mongoose.model('Post');
-
-module.exports  = function(passport){
+module.exports = function(passport){
 
     // Passport needs to be able to serialize and deserialize users to support persistent login sessions
     passport.serializeUser(function(user, done) {
-
-        // tell passport which id to use for passport
-        console.log('serializing user:',user._id);
-        return done(null, user._id);
+        console.log('serializing user:',user.username);
+        done(null, user._id);
     });
 
     passport.deserializeUser(function(id, done) {
-
-        User.findById(id, function (err, user) {
-
-            if (err) {
-                return done(err, false);
-            }
-            if(!user) {
-                return done('User not found', false);
-            }
-
-            // we found the user object provided it back to passport
-            return done(null, user);
+        User.findById(id, function(err, user) {
+            console.log('deserializing user:',user.username);
+            done(err, user);
         });
-
     });
 
     passport.use('login', new LocalStrategy({
             passReqToCallback : true
         },
-        function(req, username, password, done) {
-
-            // Check for user from the database
-            User.findOne({ username: username}, function (err, userObj) {
-
-                User.findOne({username: username}, function (err, user) {
-
-                    if(err) {
-                        return done(err, false);
+        function(req, username, password, done) { 
+            // check in mongo if a user with username exists or not
+            User.findOne({ 'username' :  username }, 
+                function(err, user) {
+                    // In case of any error, return using the done method
+                    if (err)
+                        return done(err);
+                    // Username does not exist, log the error and redirect back
+                    if (!user){
+                        console.log('User Not Found with username '+username);
+                        return done(null, false);                 
                     }
-                    if(!user) {
-                        return done('User ' + user + 'not found', false);
+                    // User exists but wrong password, log the error 
+                    if (!isValidPassword(user, password)){
+                        console.log('Invalid Password');
+                        return done(null, false); // redirect back to login page
                     }
-
-                    if (!isValidPassword(user, password)) {
-                        return done('Incorrect password', false);
-                    }
-
+                    // User and password both match, return user from done method
+                    // which will be treated like success
                     return done(null, user);
-                });
-
-                // if user NOT found
-                if (err) {
-                    return done(err, false);
                 }
-
-                // if user found
-                if (userObj) {
-                    return done('You are successfully logged in', true);
-                }
-
-                var user = new User();
-                username: username;
-                password: createHash(password)
-
-                user.save(function (err, user) {
-
-                    if(err) {
-                        return done(err, false);
-                    }
-                    if(user) {
-                        console.log('Successfully signed up user');
-                        return done(null, user);
-                    }
-                });
-            });
+            );
         }
     ));
 
@@ -101,7 +60,7 @@ module.exports  = function(passport){
                 }
                 // already exists
                 if (user) {
-                    console.log('User already exists with username: ' + username);
+                    console.log('User already exists with username: '+username);
                     return done(null, false);
                 } else {
                     // if there is no user, create the user
@@ -122,10 +81,9 @@ module.exports  = function(passport){
                     });
                 }
             });
-
         })
     );
-
+    
     var isValidPassword = function(user, password){
         return bCrypt.compareSync(password, user.password);
     };
